0c5b9788314db1d0c664451464749460c3242c56
Robbe Van Petegem <robbe@vanpetegem.me>
2021-04-17 11:30:44 +0200

    First attempt

---
+++ index.js
@@ -0,0 +1,12 @@
+const postcss = require('postcss')
+
+module.exports = (root, result) => {
+  const plugins = [];
+  root.walkAtRules('multiple-tailwind', atRule => {
+    const file =  atRule.params.length ? atRule.params : `${result.opts.file.dirname}/tailwind.config.js`
+    const tailwindcss = require("tailwindcss")(file)
+    plugins.push(...tailwindcss.plugins)
+    atRule.remove();
+  })
+  return postcss(plugins).process(root)
+};
