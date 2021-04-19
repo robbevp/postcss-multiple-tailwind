d78f5dbb1f559c008d6c9a97ae94ddef2ad33977
Robbe Van Petegem <robbe@vanpetegem.me>
2021-04-19 11:11:37 +0200

    Add opts for mode and defaultConfig to plugin

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-const postcss = require('postcss')
-const path = require("path")
+const postcss = require("postcss");
+const path = require("path");
+const fs = require("fs");
 
-module.exports = (root, result) => {
-  const plugins = [];
-  root.walkAtRules('multiple-tailwind', atRule => {
-    const file =  `${path.dirname(result.opts.from)}/${atRule.params.length ? atRule.params : 'tailwind.config.js'}`
+module.exports = (opts = {}) => {
+  /*
+   * Mode: default = manual
+   * Should be manual or auto.
+   * Auto allows user to leave out @multiple-tailwind;
+   * Manual requires a declaration of @multiple-tailwind; in every file that should be processed by tailwind.
+   */
+  const mode = opts.mode ? opts.mode : "manual";
+
+  /*
+   * defaultConfig: default = tailwind.config.js
+   * default name of the tailwind config file if no param is given
+   */
+  const defaultConfig = opts.defaultConfig
+    ? opts.defaultConfig
+    : "tailwind.config.js";
+
+  return (root, result) => {
+    let config;
+    let count = 0;
+
+    // Set config to default if mode is auto and no config is specified
+    if (mode === "auto") config = defaultConfig;
+
+    root.walkAtRules("multiple-tailwind", (atRule) => {
+      if (count > 0)
+        throw new Error(
+          `You registered '@multiple-tailwind' more than once in '${result.opts.from}'`
+        );
+      config = atRule.params.length ? atRule.params : defaultConfig;
+      atRule.remove();
+      count++;
+    });
+
+    // We silently return if there is no config given
+    if (!config) return;
+
+    const file = `${path.dirname(result.opts.from)}/${config}`;
     try {
-      const tailwindcss = require("tailwindcss")(file)
-      plugins.push(...tailwindcss.plugins)
+      fs.accessSync(file, fs.constants.F_OK);
     } catch (_) {
       throw new Error(`Cannot find config file '${file}'`);
     }
-    atRule.remove();
-  })
-  return plugins.length ? postcss(plugins).process(root, { from: undefined }) : [];
+
+    const tailwindcss = require("tailwindcss")(file);
+    return postcss(tailwindcss.plugins).process(root, { from: undefined });
+  };
 };
+
+module.exports.postcss = true;
