6b2d8e0fe72586d6e0017721ed497d2a3bf264e3
Robbe Van Petegem <robbe@vanpetegem.me>
2021-04-19 11:05:54 +0200

    Extract error function in tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -12,6 +12,18 @@ async function run(inputFile, output, opts) {
   expect(result.warnings()).toHaveLength(0);
 }
 
+async function error(inputFile, error, opts) {
+  const file = path.join(__dirname, "fixtures", inputFile);
+  const css = fs.readFileSync(file);
+  await expect(
+    postcss([plugin(opts)]).process(css, { from: file })
+  ).rejects.toThrow(
+    expect.objectContaining({
+      message: error,
+    })
+  );
+}
+
 /* Manual mode */
 it("should not transform if @multiple-tailwind is not present in default mode", async () => {
   await run(
@@ -56,43 +68,23 @@ it("should use specific default config if given in opts in auto mode", async () 
 /* Errors */
 
 it("should throw error if config does not exist", async () => {
-  const file = path.join(__dirname, "fixtures", "error-false config.css");
-  const css = fs.readFileSync(file);
-  await expect(
-    postcss([plugin()]).process(css, { from: file })
-  ).rejects.toThrow(
-    expect.objectContaining({
-      message: `Cannot find config file '${__dirname}/fixtures/undefined.config.js'`,
-    })
+  await error(
+    "error-false config.css",
+    `Cannot find config file '${__dirname}/fixtures/undefined.config.js'`
   );
 });
 
 it("should throw error if config from opts does not exist", async () => {
-  const file = path.join(__dirname, "fixtures", "manual-default.css");
-  const css = fs.readFileSync(file);
-  await expect(
-    postcss([plugin({ defaultConfig: "undefined.config.js" })]).process(css, {
-      from: file,
-    })
-  ).rejects.toThrow(
-    expect.objectContaining({
-      message: `Cannot find config file '${__dirname}/fixtures/undefined.config.js'`,
-    })
+  await error(
+    "manual-default.css",
+    `Cannot find config file '${__dirname}/fixtures/undefined.config.js'`,
+    { defaultConfig: "undefined.config.js" }
   );
 });
 
 it('should throw error if "@multiple-tailwind" is present multiple times', async () => {
-  const file = path.join(
-    __dirname,
-    "fixtures",
-    "error-multiple declarations.css"
-  );
-  const css = fs.readFileSync(file);
-  await expect(
-    postcss([plugin()]).process(css, { from: file })
-  ).rejects.toThrow(
-    expect.objectContaining({
-      message: `You registered '@multiple-tailwind' more than once in '${__dirname}/fixtures/error-multiple declarations.css'`,
-    })
+  await error(
+    "error-multiple declarations.css",
+    `You registered '@multiple-tailwind' more than once in '${__dirname}/fixtures/error-multiple declarations.css'`
   );
 });
